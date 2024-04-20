30bfb60329f8e40c342ab10efc3b442fd0bffb75
OlesaPolezhay <olesapolezaj467@gmail.com>
2024-04-20 12:43:00 +0300

    add BD and welcome message

--- src/main/java/com/example/langchallenge2/bot/commands/StartCommandHandler.java
+++ src/main/java/com/example/langchallenge2/bot/commands/StartCommandHandler.java
@@ -1,5 +1,6 @@
 package com.example.langchallenge2.bot.commands;
 
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
@@ -21,10 +22,22 @@ public class StartCommandHandler extends BotCommand
 
   @Override
   public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
+
+    String stickerWelcome = "CAACAgIAAxkBAAEEzJFmICZkvauEj6gZiDHm3a0-olqosQACuAADUomRI0m50GWI4c3YNAQ";
+
+    String messageWelcome = "\uD83D\uDC4B Привіт " + chat.getFirstName() + "\n\n" +
+        "Я - \uD83C\uDF0ELangChallenger \uD83C\uDF0E\n" +
+        "\n" +
+        "Проходь випробування, заробляй монети, та підвищуй свій рівень знань\uD83C\uDF0E\n" +
+        "\n" +
+        "Якщо потрібна буде допомога друкуй  /help.";
+
     try {
-      InputFile sticker = new InputFile( "CAACAgIAAxkBAAEEzJFmICZkvauEj6gZiDHm3a0-olqosQACuAADUomRI0m50GWI4c3YNAQ");
+      InputFile sticker = new InputFile( stickerWelcome);
       absSender.execute(new SendSticker(chat.getId().toString(), sticker));
-      absSender.execute(new SendMessage(chat.getId().toString(), " text"));
+
+      absSender.execute(new SendMessage(chat.getId().toString(), messageWelcome));
+
     } catch (TelegramApiException e) {
       throw new RuntimeException(e);
     }
---
+++ src/main/resources/bd/initBD.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS lang_quiz;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
+(
+    id         SERIAL PRIMARY KEY,
+    chat_id    INTEGER UNIQUE                NOT NULL,
+    name       VARCHAR                       NOT NULL,
+    score      INTEGER             DEFAULT 0 NOT NULL,
+    high_score INTEGER             DEFAULT 0 NOT NULL,
+    bot_state  VARCHAR                       NOT NULL
+);
+
+CREATE TABLE lang_quiz
+(
+    id             SERIAL PRIMARY KEY,
+    question       VARCHAR NOT NULL,
+    picture        VARCHAR ,
+    sticker        VARCHAR ,
+    answer_correct VARCHAR NOT NULL,
+    option1        VARCHAR NOT NULL,
+    option2        VARCHAR NOT NULL,
+    option3        VARCHAR NOT NULL
+);
